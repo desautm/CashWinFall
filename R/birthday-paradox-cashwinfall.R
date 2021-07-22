7018ef6184e8cb4ef1295392dfd6b440b9bbc039
Marc-André Désautels <desautm@hotmail.com>
2021-07-22 13:47:57 -0400

    Paradoxe anniversaires et Selbee

--- R/birthday-paradox-cashwinfall.R
+++ R/birthday-paradox-cashwinfall.R
@@ -19,11 +19,17 @@ cashwinfall <- tibble(n=id, `Probabilité de coïncidence`=p()) %>%
   pivot_longer(!n,names_to="Probabilités", values_to="proba")
 
 cashwinfall %>% ggplot(aes(x=n))+
-  geom_line(aes(y=proba,color=`Probabilités`))+
+  geom_line(aes(y=proba,color=`Probabilités`),size=1)+
   scale_x_continuous(breaks = seq(from=0,to=30000,by=5000))+
   labs(
     x = "Nombre de billets",
     y = "Probabilité"
   )+
-  theme(legend.position = "bottom")
+  theme(legend.position = "bottom")+
+  geom_hline(yintercept=0.5, color="orange", size=1)+
+  geom_vline(xintercept=3604, color="orange", size=1)+
+  annotate("text",x=4500,y=0,label = c("3604") , color="orange",size=5,fontface="bold")
 
+cashwinfall_table <- cashwinfall %>% 
+  filter(`Probabilités`=="Probabilité de coïncidence") %>% 
+  filter(abs(proba-0.5)<0.0001)
--- R/birthday-paradox.R
+++ R/birthday-paradox.R
@@ -18,15 +18,19 @@ birthday <- tibble(n=id, `Probabilité de coïncidence`=p()) %>%
   pivot_longer(!n,names_to="Probabilités", values_to="proba")
 
 birthday %>% ggplot(aes(x=n))+
-  geom_line(aes(y=proba,color=`Probabilités`))+
+  geom_line(aes(y=proba,color=`Probabilités`),size=1)+
   scale_x_continuous(breaks = seq(from=0,to=365,by=50))+
   labs(
     x = "Nombre de personnes",
     y = "Probabilité"
   )+
-  theme(legend.position = "bottom")
+  theme(legend.position = "bottom")+
+  geom_hline(yintercept=0.5, color="orange", size=1)+
+  geom_vline(xintercept=23, color="orange", size=1)+
+  annotate("text",x=30,y=0,label = c("23") , color="orange",size=5,fontface="bold")
 
 birthday_table <- birthday %>% 
   filter(Probabilités=="Probabilité de coïncidence") %>% 
   filter(n %in% c(5,10,15,20,23,25,30,40,50,60,80,100,200,300,365)) %>% 
-  select(n,proba)
+  mutate(`Probabilités` = proba) %>% 
+  select(n,`Probabilités`)
